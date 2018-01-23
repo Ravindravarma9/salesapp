06bf7e4ebf059ec5ad2a032c779908d72527ac2b
ravindra <ravin9009@gmail.com>
2018-01-23 19:37:19 +0000

    updating hello world with changes, sixth commit

--- helloworld.java
+++ helloworld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-	// Prints "Hello, Git in the terminal window"
 	System.out.println("Hello Git");
-	 System.out.println("Hello java");
+	//Prints "Hello.Helo
+	System.out.println("hello java");
     }
 
 }
